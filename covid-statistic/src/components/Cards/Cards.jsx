b1b470e09f41e7165831896d29e5ad37672b3659
Hellen <yongminni97@gmail.com>
2020-06-06 19:56:41 +0300

    add spiner for loading

--- covid-statistic/src/components/Cards/Cards.jsx
+++ covid-statistic/src/components/Cards/Cards.jsx
@@ -1,10 +1,11 @@
 import React from "react";
-import {Card, CardContent, Typography, Grid} from "@material-ui/core";
+import {Card, CardContent, Typography, Grid, CircularProgress} from "@material-ui/core";
 import styles from './Cards.module.css';
 import CountUp from "react-countup";
 import cx from 'classnames'
 const Cards = ({data: {confirmed, recovered, deaths, lastUpdate}}) => {
-    if(!confirmed) return 'Loading...'
+    if(!confirmed) return <CircularProgress />
+
     const CARDS = [
         {
             title: 'infected',
